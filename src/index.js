d9a9d3db5ec50172d0d02f1ebcbcb13c12f35795
nick-509 <>
2023-01-24 12:22:05 +0530

    redux-compelete

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <PersistGate persister={persister}>
+      <PersistGate persistor={persister}>
         <BrowserRouter>
           <App />
         </BrowserRouter>
